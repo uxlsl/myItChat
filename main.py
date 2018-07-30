862105444c04aa36b85a9514635a9b49bba083ee
songlin <songlin.lin@yunfangdata.com>
2018-07-30 10:51:25 +0800

    增加说明

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import itchat
 
 @itchat.msg_register(itchat.content.TEXT)
 def text_reply(msg):
-    return "现在没空看微信，请电话联系!"
+    return "你好，我是机器人，他现在没空看微信，请电话联系!"
 
 
 itchat.auto_login(enableCmdQR=2)
